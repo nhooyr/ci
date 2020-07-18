0aa4e2c2071a6c9b9a5324b5b92a74ca264eedf7
Anmol Sethi <hi@nhooyr.io>
2020-07-18 08:32:53 -0400

    Ignore SC1091 from shellcheck

--- lint.sh
+++ lint.sh
@@ -5,7 +5,7 @@ main() {
   . "$(dirname $0)/lib.sh"
 
   set_shell_files
-  shellcheck -e SC1090,SC2086,SC2154,SC2046 $shell_files
+  shellcheck -e SC1090,SC2086,SC2154,SC2046,SC1091 $shell_files
 
   if is_go_package; then
     go vet ./...
